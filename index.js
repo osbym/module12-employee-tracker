e7faadb4b67e6b4597b3b5cede7c6d9afb37de7f
osbym <marosb79@yahoo.com>
2022-01-27 14:16:52 -0600

    updated index.js

---
+++ index.js
@@ -0,0 +1,50 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
+
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "employee_trackerDB"
+});
+
+connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+  
+    console.log("connected as id " + connection.threadId);
+    start();
+  });
+
+function start() {
+    inquirer
+      .prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a department",
+          "Add a role",

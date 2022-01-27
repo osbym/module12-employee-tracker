7ecd11596c4cdeba333cfd9aef92ba0e560842eb
osbym <marosb79@yahoo.com>
2022-01-27 14:21:24 -0600

    updated index.js file

--- index.js
+++ index.js
@@ -210,3 +210,109 @@ function viewRoles() {
                 choices: function() {
                  let rolesArray = [];
                     result.forEach(result => {
+                        rolesArray.push(
+                            result.title
+                        );
+                    })
+                    return rolesArray;
+                  }
+              },
+            //   {
+            //     name: "isManager",
+            //     type: "list",
+            //     message: "Is this employee a manager?",
+            //     choices: ["YES", "NO"]
+            //   }
+            ]) 
+    // in order to get the id here, i need a way to grab it from the departments table 
+            .then(function(answer) {
+                console.log(answer);
+                const role = answer.roleName;
+                connection.query('SELECT * FROM role', function(err, res) {
+                    if (err) throw (err);
+                    let filteredRole = res.filter(function(res) {
+                        return res.title == role;
+                    });
+                
+                
+                            
+                    let query = "INSERT INTO employee (first_name, last_name, role_id) VALUES (?, ?)";
+                    let values = [answer.firstName, answer.lastName, answer.role_id]
+                    console.log(values);
+                    
+                    connection.query(query, values,
+                        function(err, res, fields) {
+                            console.log(`You have added this employee: ${(values[0]).toUpperCase()}.`)
+                        })
+                    viewEmployees();
+                           
+                })
+            })
+        })
+           
+       
+    }
+    
+    function updateRole() {
+        connection.query('SELECT * FROM employee', function(err, result) {
+            if (err) throw (err);
+        inquirer
+            .prompt([
+              {
+                name: "employeeName",
+                type: "list",
+    // is there a way to make the options here the results of a query that selects all departments?`
+                message: "Which employee's role is changing?",
+                choices: function() {
+                 employeeArray = [];
+                    result.forEach(result => {
+                        employeeArray.push(
+                            result.last_name
+                        );
+                    })
+                    return employeeArray;
+                  }
+              }
+              ]) 
+    // in order to get the id here, i need a way to grab it from the departments table 
+            .then(function(answer) {
+                console.log(answer);
+                const name = answer.employeeName;
+            
+                connection.query("SELECT * FROM role", function(err, res) {
+                        inquirer
+                        .prompt ([
+                            {
+                                name: "role",
+                                type: "list",
+                                message: "What is their new role?",
+                                choices: function() {
+                                    rolesArray = [];
+                                    res.forEach(res => {
+                                        rolesArray.push(res.title)
+                                    })
+                                    return rolesArray;
+                                }
+                            }
+                        ]).then(function(rolesAnswer) {
+                            const role = rolesAnswer.role;
+                            console.log(rolesAnswer.role);
+                            connection.query('SELECT * FROM role WHERE title = ?', [role], function(err, res) {
+                                if (err) throw (err);
+                                    let roleId = res[0].id;
+                                    let query = "UPDATE employee SET role_id ? WHERE last_name ?";
+                                    let values = [roleId, name]
+                                    console.log(values);
+                                    connection.query(query, values,
+                                        function(err, res, fields) {
+                                            console.log(`You have updated ${name}'s role to ${role}.`)
+                                        });
+                                viewEmployees();
+                            });
+                        });
+                    });
+           });
+    });
+    
+    };
+

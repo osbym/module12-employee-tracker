032e82d3a117e6691800d2cc586750b595e42aa1
osbym <marosb79@yahoo.com>
2022-01-27 14:18:35 -0600

    added employees

--- index.js
+++ index.js
@@ -48,3 +48,54 @@ function start() {
           "View all employees",
           "Add a department",
           "Add a role",
+          "Add an employee",
+          "Update employee role",
+          "Exit"
+        ]
+      })
+    .then(function(answer) {
+        if (answer.action === 'View all departments') {
+            viewDepartments();
+        } else if (answer.action === 'View all roles') {
+            viewRoles();
+        } else if (answer.action === 'View all employees') {
+            viewEmployees();
+        } else if (answer.action === 'Add a department') {
+            addDepartment();
+        } else if (answer.action === 'Add a role') {
+            addRole();
+        } else if (answer.action === 'Add an employee') {
+            addEmployee();
+        } else if (answer.action === 'Update employee role') {
+            updateRole();
+        }
+        else if (answer.action === 'Exit') {
+            connection.end();
+        }
+    });
+}
+
+
+function viewDepartments() {
+    var query = "SELECT * FROM department";
+      connection.query(query, function(err, res) {
+          if(err) throw (err);
+        res.forEach(department => {
+            console.table(`ID: ${department.id} | Name: ${department.name}`)
+        });
+        connection.end();
+    });
+};
+
+function viewRoles() {
+    var query = "SELECT * FROM role";
+        connection.query(query, function(err, res) {
+            if(err) throw (err);
+            // console.log(`ROLES:`)
+        res.forEach(role => {
+            console.table(`ID: ${role.id} | Title: ${role.title} | Salary: ${role.salary} | Department ID: ${role.department_id}`);
+        });
+        });
+        connection.end();
+    };
+    
